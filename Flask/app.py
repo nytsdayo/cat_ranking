4f41c5d9be561bf680c55bac9d973c25085bb3a8
dgs7101 <daigo.t.shimizu@gmail.com>
2024-02-20 05:59:17 +0900

    Modify json endpoints

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
+cat_list_file_name = 'cat_breeds_data.json'
+
+# 猫の画像と品種のリストを取得    
+with open(cat_list_file_name, 'r') as file:
+    cats_list = json.load(file)
+    
 # トーナメントの状態を格納
 tournament = {
     'cats': [],
@@ -10,26 +17,21 @@ tournament = {
     'results': []
 }
 
-# NUM_OF_CAT_TYPE = 32
-# cats_list = [i for i in range(0, NUM_OF_CAT_TYPE)]
-
 # 取得した猫のidリストを基にトーナメントの状態を初期化し、最初のペアを返す
 @app.route('/init_tournament', methods=['POST'])
 def init_tournament():
-    # 猫の画像のidリストを取得
-    cats_list = request.json.get('cats', [])
     if not cats_list:
         return jsonify({'error': 'cats list is required'}), 400
-    
     # トーナメントの状態を初期化
     tournament['cats'] = cats_list
-    if len(cats_list) % 2 == 1:
-        tournament['current_matches'] = [(cats_list[i], cats_list[i+1]) for i in range(0, len(cats_list)-1, 2)]
-        seed_cat = tournament['cats'].pop(len(cats_list)-1)
+    num = len(cats_list)
+    if num % 2 == 1:
+        tournament['current_matches'] = [(cats_list[i], cats_list[i+1]) for i in range(0, num-1, 2)]
+        seed_cat = tournament['cats'].pop(num-1)
         tournament['next_round'] = [seed_cat]
     else:
-        tournament['current_matches'] = [(cats_list[i], cats_list[i+1]) for i in range(0, len(cats_list), 2)]
-        tournament['next_round'] = []   
+        tournament['current_matches'] = [(cats_list[i], cats_list[i+1]) for i in range(0, num, 2)]
+        tournament['next_round'] = []
     tournament['results'] = []
 
     # 最初のペアを返す
@@ -39,7 +41,9 @@ def init_tournament():
 @app.route('/current_match', methods=['GET'])
 def current_match():
     if tournament['current_matches']:
-        return jsonify({'match': tournament['current_matches'][0]})
+        print (tournament['current_matches'][0][0]['breed_id'])
+        return jsonify({'breed_id_1': tournament['current_matches'][0][0]['breed_id'], 'image_url_1': tournament['current_matches'][0][0]['image_url'],
+                        'breed_id_2': tournament['current_matches'][0][1]['breed_id'], 'image_url_2': tournament['current_matches'][0][1]['image_url']})
     else:
         return jsonify({'error': 'No current match available'}), 404
 
@@ -49,16 +53,13 @@ def select_winner():
     # ペアの勝敗を取得
     data = request.json
     winner, loser = data.get('winner'), data.get('loser')
-    
     if not winner or not loser or winner == loser:
         return jsonify({'error': 'Invalid winner or loser'}), 400
-    current_match_pair = tournament['current_matches'][0]
-    if winner not in current_match_pair or loser not in current_match_pair:
-        return jsonify({'error': 'Winner or loser does not match current participants'}), 400
-
+    winner_cat = next((cat for cat in cats_list if cat['breed_id'] == winner), None)
+    loser_cat = next((cat for cat in cats_list if cat['breed_id'] == loser), None)
     # 勝者を次のラウンドに保存し、敗者をresultsに保存
-    tournament['next_round'].append(winner)
-    tournament['results'].append(loser)
+    tournament['next_round'].append(winner_cat)
+    tournament['results'].append(loser_cat)
     
     # 現在のマッチを消去
     tournament['current_matches'].pop(0)
@@ -80,7 +81,10 @@ def select_winner():
         else:
             # トーナメントが終了したら、その結果を降順に出力
             tournament['results'].append(tournament['next_round'][0])
-            final_results = {'results': tournament['results'].reverse()}
+            tournament['results'].reverse()
+            final_results = {'cat': [tournament['results'][0], tournament['results'][1],
+                                     tournament['results'][2], tournament['results'][3]]}
+
             return jsonify(final_results)
 
 if __name__ == '__main__':
