77eba773ac1e22216cc1d1b5d5aec8a0524fb5ce
nytsdayo <nyantsutsu@gmail.com>
2024-02-24 04:39:13 +0900

    main menu act(背景画像の設定、画像をボタンとして設定）

--- flutter/nekosort/lib/main.dart
+++ flutter/nekosort/lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nekomash',
+      title: 'Neko',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Nekomash'),
+      home: const MyHomePage(title: 'Neko sort'),
     );
   }
 }
@@ -32,26 +32,48 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(widget.title, style: TextStyle(color: Colors.red)),
+      ),
+      body: Container(
+        width: double.infinity,
+        // 背景画像を指定
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background_base_image.jpg"),
+            fit: BoxFit.none,
+          ),
         ),
-        body: Column(
+        // ボタン要素
+        child: Column(
+          // 要素の位置を指定
+          mainAxisAlignment: MainAxisAlignment.center, // 中央寄せ（縦）
           children: [
-            OutlinedButton(
-              child: const Text('ランキングを付ける'),
-              style: OutlinedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                side: const BorderSide(),
-              ),
-              onPressed: () {
+            // Inkwellで画像をタップ可能にする
+            InkWell(
+              // 画像タップ時の挙動
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => SortPage()),
                 );
-              }, //ソート画面に遷移するようにする
+              },
+              // コンテナ内の画像を指定
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/cat_hands.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
